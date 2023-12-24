383b25103a6afdce60627cd22a772b069afa019b
nam <npnam031101@gmail.com>
2023-12-24 09:29:00 -0500

    update message

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,29 +1,26 @@
 const userService = require('../services/userService');
 const _ = require('lodash');
 
+
 exports.createUser = async (req, res) => {
     try {
-        const isEmailAlreadyInUse = await userService.isEmailAlreadyInUse(req.body.email);
-        if (isEmailAlreadyInUse) {
-            return res.status(400).json({ status: 'error', message: "Email is already in use" });
-        }
-
-        const hashedPassword = await userService.hashPassword(req.body.password);
-        console.log(hashedPassword);
-
-        const savedUser = await userService.createUser({
-            fullName: req.body.fullName,
-            email: req.body.email,
-            password: hashedPassword,
-            phone: req.body.phone,
-            profilePicture: req.body.profilePicture,
+        const { fullName, email, password, phone, profilePicture } = req.body;
+        const isEmailAlreadyInUse = await userService.isEmailAlreadyInUse(email);
+        if (isEmailAlreadyInUse) throw { status: 400, message: 'Email is already in use' };
+        const hashedPassword = await userService.hashPassword(password);
+        const savedUser = await userService.createUser(
+            { fullName, email, password: hashedPassword, phone, profilePicture }
+        );
+        return res.status(200).json({
+            status: 'success',
+            data: savedUser,
+            message: 'User created successfully'
         });
-
-        res.status(200).json({ status: 'success', data: savedUser });
     } catch (error) {
-        res.status(400).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
+
 
 exports.getAllUsers = async (req, res) => {
     const currentPage = parseInt(req.query.currentPage) || 1;
@@ -41,66 +38,79 @@ exports.getAllUsers = async (req, res) => {
 
     try {
         const users = await userService.getAllUsers(currentPage, pageSize, filters, sortBy, sortOrder);
-        res.status(200).json({ status: 'success', data: users });
+        return res.status(200).json({
+            status: 'success',
+            data: users,
+            message: 'Users retrieved successfully'
+        });
     } catch (error) {
-        res.status(400).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
+
 
 exports.getUserById = async (req, res) => {
     try {
         const user = await userService.getUserById(req.params.id);
-        res.status(200).json({ status: 'success', data: user });
+        return res.status(200).json({
+            status: 'success',
+            data: user,
+            message: 'User retrieved successfully'
+        });
     } catch (error) {
-        res.status(400).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
+
 
 exports.updateUser = async (req, res) => {
     try {
         const { email, password, ...updateData } = req.body;
-        if (email || password) {
-            return res.status(400).json({ status: 'error', message: 'Cannot update email or password directly.' });
-        }
+        if (email || password) throw { status: 400, message: 'Invalid update request' };
         const updatedUser = await userService.updateUser(req.params.id, updateData);
-        res.status(200).json({ status: 'success', data: updatedUser });
+        return res.status(200).json({
+            status: 'success',
+            data: updatedUser,
+            message: 'User updated successfully'
+        });
     } catch (error) {
-        res.status(400).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
+
 
 exports.deleteUser = async (req, res) => {
     try {
         const deleteUser = await userService.deleteUser(req.params.id);
-        res.status(200).json({ status: 'success', data: deleteUser, message: 'User deleted successfully', });
+        return res.status(200).json({
+            status: 'success',
+            data: deleteUser,
+            message: 'User deleted successfully'
+        });
     } catch (error) {
-        res.status(400).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
+
 
 // Auth Controller
 exports.register = async (req, res) => {
     try {
-        const isEmailAlreadyInUse = await userService.isEmailAlreadyInUse(req.body.email);
-        if (isEmailAlreadyInUse) {
-            return res.status(400).json({ status: 'error', message: "Email is already in use" });
-        }
-
-        const hashedPassword = await userService.hashPassword(req.body.password);
+        const { fullName, email, password, phone, profilePicture } = req.body;
+        const isEmailAlreadyInUse = await userService.isEmailAlreadyInUse(email);
+        if (isEmailAlreadyInUse) throw { status: 400, message: 'Email is already in use' };
+        const hashedPassword = await userService.hashPassword(password);
         const user = await userService.createUser({
-            fullName: req.body.fullName,
-            email: req.body.email,
-            password: hashedPassword,
-            phone: req.body.phone,
-            profilePicture: req.body.profilePicture,
-            role: 'user',
+            fullName, email, password: hashedPassword, phone, profilePicture, role: 'user'
         });
-
         const userData = _.pick(user, ['_id', 'email', 'fullName', 'profilePicture', 'phone']);
-
-        res.status(200).json({ status: 'success', data: userData });
+        return res.status(200).json({
+            status: 'success',
+            data: userData,
+            message: 'User registered successfully'
+        });
     } catch (error) {
-        res.status(400).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
 
@@ -115,22 +125,24 @@ exports.login = async (req, res) => {
         const refresh_token = userService.generateRefreshToken(user._id);
         res.cookie('refresh_token', refresh_token, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
         const userData = _.pick(user, ['_id', 'email', 'fullName', 'profilePicture', 'phone', 'role']);
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             data: { access_token, user: userData },
             message: 'Login successful'
         });
     } catch (error) {
-        res.status(error.status || 500).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
 
 exports.logout = async (req, res) => {
     try {
-        res.clearCookie('refresh_token');
-        res.status(200).json({ status: 'success', message: 'Logout successful' });
+        return res.clearCookie('refresh_token').status(200).json({
+            status: 'success',
+            message: 'Logout successful'
+        });
     } catch (error) {
-        res.status(error.status || 500).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
 
@@ -142,13 +154,13 @@ exports.refreshToken = async (req, res) => {
         if (!user) throw { status: 401, message: 'Invalid refresh token' };
         const newAccessToken = userService.generateAccessToken(user._id);
         const userData = _.pick(user, ['_id', 'email', 'fullName', 'profilePicture', 'phone', 'role']);
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             data: { access_token: newAccessToken, user: userData },
             message: 'Token refreshed successfully'
         });
     } catch (error) {
-        res.status(error.status || 500).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
 
@@ -157,8 +169,28 @@ exports.fetchAccount = async (req, res) => {
         const user = await userService.getUserById(req.user.userId);
         if (!user) throw { status: 401, message: 'Unauthorized' };
         const userData = _.pick(user, ['_id', 'email', 'fullName', 'profilePicture', 'phone', 'role']);
-        res.status(200).json({ status: 'success', data: { user: userData } });
+        return res.status(200).json({ status: 'success', data: { user: userData } });
     } catch (error) {
-        res.status(error.status || 500).json({ status: 'error', message: error.message });
+        return res.status(error.status || 500).json({ status: 'error', message: error.message });
     }
 };
+
+// Viết thêm
+
+// API đổi mật khẩu
+// app.put('/api/change-password/:userId', (req, res) => {
+//     const userId = parseInt(req.params.userId, 10);
+//     const { newPassword } = req.body;
+
+//     // Tìm người dùng trong mảng
+//     const user = users.find((u) => u.id === userId);
+
+//     if (!user) {
+//         return res.status(404).json({ error: 'Người dùng không tồn tại' });
+//     }
+
+//     // Đổi mật khẩu
+//     user.password = newPassword;
+
+//     res.json({ message: 'Đổi mật khẩu thành công' });
+// });
