c757ddd18eb604410b598aaa53b64d001fc7fe42
nam <npnam031101@gmail.com>
2024-01-15 07:29:18 -0500

    update api upload

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authenticateUser = require('../middlewares/authenticateUser');
 const uploadToCloudinary = require('../middlewares/uploadToCloudinary');
-
+const cloudinary = require('cloudinary').v2;
 
 router.post('/', authenticateUser, userController.createUser);
 router.get('/', authenticateUser, userController.getAllUsers);
@@ -19,12 +19,22 @@ router.get('/auth/refresh', userController.refreshToken);
 router.get('/auth/account', authenticateUser, userController.fetchAccount);
 
 
-router.post('/upload-cloudinary', uploadToCloudinary, (req, res) => {
+router.post('/upload/cloudinary', uploadToCloudinary, (req, res) => {
     const imageUrl = req.cloudinaryResult.secure_url;
     res.status(200).json({
         message: 'Image uploaded successfully',
         imageUrl: imageUrl,
     });
+});
+
+router.get('/upload/cloudinary', async (req, res) => {
+    try {
+        const result = await cloudinary.api.resources({ type: 'upload' });
+        res.status(200).json({ data: result });
+    } catch (error) {
+        console.error('Cloudinary Error:', error);
+        res.status(500).json({ message: 'Error retrieving images from Cloudinary', error: error });
+    }
 });
 
 
